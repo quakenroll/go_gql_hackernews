652254fd4e39404dc7a6739b995b5065a2d13607
m <m>
2021-06-22 05:09:08 +0900

    ...

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/quakenroll/go_gql_hackernews
 go 1.16
 
 require (
+	cloud.google.com/go v0.84.0 // indirect
 	github.com/766b/go-outliner v0.0.0-20180511142203-fc6edecdadd7 // indirect
 	github.com/99designs/gqlgen v0.13.0
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-chi/chi v3.3.2+incompatible
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang-migrate/migrate v3.5.4+incompatible
@@ -13,5 +14,6 @@ require (
 	//github.com/golang-migrate/migrate/v4 v4.14.1 // indirect
 	github.com/vektah/gqlparser/v2 v2.1.0
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
+	golang.org/x/oauth2 v0.0.0-20210615190721-d04028783cf1
 
 )
--- server.go
+++ server.go
@@ -20,6 +20,9 @@ import (
 
 	//"github.com/quakenroll/go_gql_hackernews/internal/auth"
 	//_ "github.com/quakenroll/go_gql_hackernews/internal/auth"
+	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -28,6 +31,8 @@ import (
 	gqlgen_hackernews "github.com/quakenroll/go_gql_hackernews/graph/generated"
 	"github.com/quakenroll/go_gql_hackernews/internal/auth"
 	database "github.com/quakenroll/go_gql_hackernews/internal/pkg/db/mysql"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -36,14 +41,75 @@ import (
 
 /*
 google oauth2
-	client id : 1017766501798-6cq9t9iirccri93udjffg9s8a8esrn28.apps.googleusercontent.com
-	client pass : ayoClDJ9bgDFNbjKPq8TzOO9
+	ClientID : 1017766501798-6cq9t9iirccri93udjffg9s8a8esrn28.apps.googleusercontent.com
+	ClientSecret : ayoClDJ9bgDFNbjKPq8TzOO9
+	callback : http://localhost:8081/auth/google/callback
 	port : 8081
 */
 
 const defaultPort = "8081"
 
+var googleOauthConfig = oauth2.Config{
+	RedirectURL:  "https://localhost:8081/auth/google/callback",
+	ClientID:     "1017766501798-6cq9t9iirccri93udjffg9s8a8esrn28.apps.googleusercontent.com",
+	ClientSecret: "ayoClDJ9bgDFNbjKPq8TzOO9",
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+	Endpoint:     google.Endpoint,
+}
+
+type googleLoginHandler struct {
+}
+
+func (d googleLoginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL("MyAuthVerificationString")
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+type googleAuthCallbackHandler struct {
+}
+
+func (d googleAuthCallbackHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	value := r.FormValue("state")
+	if value != "MyAuthVerificationString" {
+		log.Println("invalid google oauth state", value)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	data, err := getGoogleUserInfo(r.FormValue("code"))
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprint(w, string(data))
+}
+
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
+
+func getGoogleUserInfo(code string) ([]byte, error) {
+
+	token, err := googleOauthConfig.Exchange(context.Background(), code)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to Exchange %s\n", err.Error())
+	}
+
+	resp, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to Get UserInfo from Google %s\n", err.Error())
+	}
+	return ioutil.ReadAll(resp.Body)
+}
+
 func main() {
+
+	//generatepem.GenFile()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -55,10 +121,13 @@ func main() {
 	database.InitDB()
 	database.Migrate()
 	server := handler.NewDefaultServer(gqlgen_hackernews.NewExecutableSchema(gqlgen_hackernews.Config{Resolvers: &graph.Resolver{}}))
+
+	router.Handle("/auth/google/login", googleLoginHandler{})
+	router.Handle("/auth/google/callback", googleAuthCallbackHandler{})
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", server)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	log.Printf("connect to https://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServeTLS(":"+port, "localhost.crt", "localhost.key", router))
 
 }
